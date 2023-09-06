c90f7dbc74f324fc740b2ebc7d3afdcdd6a80fb6
ARJUN RAHUL VIJI <avrahulkanna17@gmail.com>
2023-09-06 23:44:40 +0530

    Initial Files with Functions.

---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+from map_data import add_new_info, authenticate_me, get_my_info, get_creative_map
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix="lord ", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    await bot.tree.sync()
+
+
+@bot.command()
+async def random_creative(ctx):
+    print("Yes")
+    await ctx.send("Creative Maps")
+
+@bot.command()
+async def save_me(ctx, user, pwd):
+    await ctx.message.delete()
+    add_new_info(ctx.message.author.id, user, pwd)
+    await ctx.send("Stored successfully")
+
+@bot.command()
+async def login(ctx):
+    result = get_my_info(ctx.message.author.id)
+    if result == {}:
+        await ctx.send("Please Register your Details")
+    else:
+        username = result["username"]
+        password = result["password"]
+        authenticate_me(username, password)
+        await ctx.send("Successfully Logged In")
+
+@bot.command()
+async def creative(ctx):
+    get_creative_map()
+
+token = "MTE0NDEwNzczNDAzMjE4NzQxMg.Gf8azT.s7xL3BEpt0FP1frv-CZ2psQlo4STUFFDPi_fiY"
+
+# @bot.event
+# async def on_message(message):
+#     if message.content.startswith('lord save_me'):
+#         await message.delete()
+#         await asyncio.sleep(0.001)
+#         # add_new_info(message.author.id, message.content.split(" ")[2], message.content.split(" ") [3])
+
+
+# Run the bot
+bot.run(token)  # Replace with your bot's token
---
+++ map_data.py
@@ -0,0 +1,128 @@
+import time
+import os
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+from dotenv import load_dotenv, find_dotenv
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+# options = Options()
+# options.add_argument("--headless")
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
+
+load_dotenv(find_dotenv())
+
+def authenticate_me(user, pwd):
+    CHROME_URL = r"E:\chromedriver-win64\chromedriver.exe"
+    URL = "https://twitter.com/home"
+    driver = Chrome(service=Service(CHROME_URL))
+    driver.get(URL)
+
+    time.sleep(5)
+
+    EMAIL_XPATH = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input"
+    NEXT_XPATH = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]"
+    SECURE_PATH = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input"
+    PASSWORD_PATH = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input"
+    FURTHER = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div"
+    LOGIN_XPATH = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div"
+    email_input = driver.find_element(By.XPATH, EMAIL_XPATH)
+    email_input.send_keys(user)
+    next_button = driver.find_element(By.XPATH, NEXT_XPATH)
+    next_button.click()
+    time.sleep(3)
+
+    try:
+        password_input = driver.find_element(By.XPATH, PASSWORD_PATH)
+        password_input.send_keys(pwd)
+
+        login_button = driver.find_element(By.XPATH, LOGIN_XPATH)
+        login_button.click()
+
+        time.sleep(5)
+        search_elem_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div/label/div[2]/div/input"
+        search_elem = driver.find_element(By.XPATH, search_elem_xpath)
+        print("Found")
+        search_elem.click()
+        time.sleep(4)
+        search_elem.send_keys("FGCreativeMaps")
+        time.sleep(4)
+        that_profile_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[2]/div/div[4]/div"
+        click_profile = driver.find_element(By.XPATH, that_profile_xpath)
+        click_profile.click()
+
+        descriptions = []
+
+        DESC_XPATH = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/section/div/div/div[2]/div/div/article/div/div/div[2]/div[2]/div[2]/div/span[2]"
+
+    # time.sleep(12)
+    # CLOSE_NOTIF_XPATH = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[1]/div/div/div/div[1]/div/div"
+    #
+    # no_notif = driver.find_element(By.XPATH, CLOSE_NOTIF_XPATH)
+    # no_notif.click()
+
+        time.sleep(10)
+
+        while True:
+            actions = ActionChains(driver)
+            actions.scroll_by_amount(delta_x=1000, delta_y=1500).perform()
+            DESC_XPATH = f"/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/section/div/div"
+            map_posts = driver.find_element(By.XPATH, DESC_XPATH)
+            codes = map_posts.text.splitlines()[7]
+            level_name = map_posts.text.splitlines()[4]
+            print(f"{level_name} - {codes}")
+            time.sleep(2)
+
+    except:
+        secure = driver.find_element(By.XPATH, SECURE_PATH)
+        secure.send_keys("Nibelic")
+
+        next = driver.find_element(By.XPATH, FURTHER)
+        next.click()
+
+        time.sleep(2)
+        password_input = driver.find_element(By.XPATH, PASSWORD_PATH)
+        password_input.send_keys("avrK_2004")
+
+        login_button = driver.find_element(By.XPATH, LOGIN_XPATH)
+        login_button.click()
+
+    time.sleep(15)
+
+
+
+def add_new_info( reference, username, password):
+    with open("../.env", "w") as file:
+        file.write(f"{reference}={username}:{password}")
+    print("File modified successfully")
+
+def get_my_info(myID):
+    savedDetails = os.environ.get(str(myID))
+    if savedDetails is None:
+        return {}
+    else:
+        return {
+            "username": savedDetails.split(":")[0],
+            "password": savedDetails.split(":")[1]
+        }
+
+def get_creative_map():
+    CHROME_URL = r"E:\chromedriver-win64\chromedriver.exe"
+    URL = "https://twitter.com/FGCreativeMaps"
+    driver = Chrome(service=Service(CHROME_URL))
+    driver.get(URL)
+    time.sleep(5)
+    actions = ActionChains(driver)
+    while True:
+        actions.scroll_by_amount(delta_x=1000, delta_y=1500).perform()
+        DESC_XPATH = f"/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/section/div/div"
+        map_posts = driver.find_element(By.XPATH, DESC_XPATH)
+        codes = map_posts.text.splitlines()[7]
+        level_name = map_posts.text.splitlines()[4]
+        print(f"{level_name} - {codes}")
+        time.sleep(2)
+
